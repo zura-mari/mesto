286be9802a6b4d410febd82fc6847438da8153c8
Zura <zuramariwedding@gmail.com>
2020-12-20 17:12:17 +0300

    поправил скрипти

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,34 @@
 let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
-let addButton = profile.querySelector('.profile__add-button');
 let popup = document.querySelector('.popup');
 let popupContainer = popup.querySelector('.popup__container');
-let popupOpend = popup.querySelector('.popup_opened');
 let popupCloseButton = popup.querySelector('.popup__close-button');
-let popupSaveButton = popup.querySelector('.popup__save-button');
-// let cardContainer = document.querySelector('.card__container');
-// let cardsSection = document.querySelector('.cards__section');
-// let cardLikeButton = cardContainer.querySelector('.card__like');
+let nameInput = document.querySelector('.popup__text_type_name');
+let aboutInput = document.querySelector('.popup__text_type_about');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
-
-editButton.addEventListener('click', function () {
+function editInfo() {
     popup.classList.add('popup_opened');
-});
+    nameInput.value = profileTitle.textContent;
+    aboutInput.value = profileSubtitle.textContent;
+}
+editButton.addEventListener('click', editInfo);
 
-popupCloseButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
+function popupClose(){
     popup.classList.remove('popup_opened');
-});
-
-popupContainer.addEventListener('click', function (evt) {
-    evt.preventDefault();
-});
+}
+popupCloseButton.addEventListener('click', popupClose)
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__text_type_name');
-    let aboutInput = document.querySelector('.popup__text_type_about');
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = aboutInput.value;
-
+    popupClose();
 }
+popupContainer.addEventListener('submit', formSubmitHandler);
 
-popupSaveButton.addEventListener('click', formSubmitHandler);
-popupSaveButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popup.classList.remove('popup_opened');
-});
 
 
 
